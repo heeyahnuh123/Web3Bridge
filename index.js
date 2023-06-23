0dc9c25e77e9e913216238dfa848fbcb94e102be
heeyahnuh123 <heeyahnuh@gmail.com>
2023-06-23 13:36:33 +0100

    second commit

--- index.js
+++ index.js
@@ -8,31 +8,32 @@ let allocatedDevelopers = {
 let allocatedFacilitators = [];
 
 function allocateConferenceRoom() {
-  if (allocatedFacilitators.length < 1 && getTotalAllocatedDevelopers() < 12) {
+  if (allocatedFacilitators.length < 1 && getTotalAllocatedDevelopers() < 24) {
     allocatedFacilitators.push("Facilitator");
-    alert("Conference room allocated for Facilitator.");
+    document.getElementById("conference-room").textContent = "Conference room allocated for Facilitator.";
   } else {
-    alert("Cannot allocate conference room at the moment.");
+    document.getElementById("conference-room").textContent = "Cannot allocate conference room at the moment.";
   }
 }
 
 function allocateBedroom(bedroomNumber) {
-  if (getTotalAllocatedDevelopers() >= 12) {
+  if (getTotalAllocatedDevelopers() >= 24) {
     alert("Cannot allocate more developers. All rooms are occupied.");
     return;
   }
 
-  let developersToAllocate = prompt("Enter developers' names (comma-separated):");
-  developersToAllocate = developersToAllocate.split(",").map(dev => dev.trim());
+  let developerName = prompt("Enter developer's name:");
+  let skillset = prompt("Enter developer's skillset:");
+  let gender = prompt("Enter developer's gender:");
 
-  if (developersToAllocate.length === 4 && validateDevelopers(developersToAllocate)) {
-    let bedroom = document.querySelector("#bedroom-" + bedroomNumber);
-    bedroom.innerHTML += "<p>" + developersToAllocate.join(", ") + "</p>";
-    allocatedDevelopers.smartContract.push(...developersToAllocate.slice(0, 2));
-    allocatedDevelopers.frontend.push(developersToAllocate[2]);
-    allocatedDevelopers.serverSide.push(developersToAllocate[3]);
+  let developer = `${developerName}, ${skillset}, ${gender}`;
+
+  if (validateDeveloper(developer)) {
+    let bedroom = document.querySelector(`#bedroom-${bedroomNumber}`);
+    bedroom.innerHTML += `<p>${developer}</p>`;
+    assignDeveloperToBedroom(developer, skillset, bedroomNumber);
   } else {
-    alert("Invalid number or skills of developers. Please try again.");
+    alert("Invalid developer or incorrect format. Please try again.");
   }
 }
 
@@ -40,34 +41,39 @@ function getTotalAllocatedDevelopers() {
   return Object.values(allocatedDevelopers).reduce((total, devs) => total + devs.length, 0);
 }
 
-function validateDevelopers(developers) {
-  let smartContractCount = 0;
-  let frontendCount = 0;
-  let serverSideCount = 0;
+function validateDeveloper(developer) {
+  let [_, skillset, gender] = developer.split(", ").map(item => item.trim());
 
-  developers.forEach(dev => {
-    if (allocatedDevelopers.smartContract.includes(dev)) {
-      smartContractCount++;
-    }
-    if (allocatedDevelopers.frontend.includes(dev)) {
-      frontendCount++;
-    }
-    if (allocatedDevelopers.serverSide.includes(dev)) {
-      serverSideCount++;
-    }
-  });
+  if (allocatedDevelopers.smartContract.includes(developer) || 
+      allocatedDevelopers.frontend.includes(developer) || 
+      allocatedDevelopers.serverSide.includes(developer)) {
+    return false;
+  }
 
-  return (
-    smartContractCount <= 2 &&
-    frontendCount <= 1 &&
-    serverSideCount <= 1 &&
-    validateGender(developers)
-  );
+  let sameSkillsetDevelopers = allocatedDevelopers[skillset];
+  if (sameSkillsetDevelopers.length >= 3) {
+    return false;
+  }
+
+  if (gender === "Male") {
+    if (sameSkillsetDevelopers.some(dev => dev.includes("Female"))) {
+      return false;
+    }
+  } else if (gender === "Female") {
+    if (sameSkillsetDevelopers.some(dev => dev.includes("Male"))) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-function validateGender(developers) {
-  let males = developers.filter(dev => dev.toLowerCase().includes("male"));
-  let females = developers.filter(dev => dev.toLowerCase().includes("female"));
+function assignDeveloperToBedroom(developer, skillset, bedroomNumber) {
+  let developersList = allocatedDevelopers[skillset];
+  developersList.push(developer);
 
-  return !(females.length > 0 && males.length > 0);
+  if (developersList.length === 3) {
+    let bedroom = document.querySelector(`#bedroom-${bedroomNumber}`);
+    bedroom.innerHTML += "<p>Bedroom is full.</p>";
+  }
 }
